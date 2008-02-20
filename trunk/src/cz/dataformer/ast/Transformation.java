21ff8be3cb6e6bfee65e45d96776376ee0805efb
misho <misho@b289833f-8e39-0410-9741-99da841e8067>
2008-02-20 05:10:46 +0000

    fixed: package and imports were not saved into AST node

--- trunk/src/cz/dataformer/ast/Transformation.java
+++ trunk/src/cz/dataformer/ast/Transformation.java
@@ -3,6 +3,7 @@ package cz.dataformer.ast;
 import java.util.List;
 
 import cz.dataformer.DataFormerNode;
+import cz.dataformer.ast.expression.NameExpression;
 import cz.dataformer.ast.record.RecordDeclaration;
 import cz.dataformer.ast.statement.ConnectStatement;
 
@@ -13,14 +14,18 @@ public class Transformation extends DataFormerNode {
 	public List<RecordDeclaration> records;
 	public List<ComponentVariableDeclaration> variables;
 	public List<ConnectStatement> graph;
+	public NameExpression packageName;
+	public List<ImportDeclaration> imports;
 
-	public Transformation(int line, int column, String name, List<ComponentDeclaration> components, List<RecordDeclaration> records, List<ComponentVariableDeclaration> variables, List<ConnectStatement> graph) {
+	public Transformation(int line, int column, String name, List<ComponentDeclaration> components, List<RecordDeclaration> records, List<ComponentVariableDeclaration> variables, List<ConnectStatement> graph, NameExpression packageName, List<ImportDeclaration> imports) {
 		super(line, column);
 		this.name = name;
 		this.components = components;
 		this.records = records;
 		this.variables = variables;
 		this.graph = graph;
+		this.packageName = packageName;
+		this.imports = imports;
 	}
 	
 	public List<ComponentDeclaration> getComponents() {
